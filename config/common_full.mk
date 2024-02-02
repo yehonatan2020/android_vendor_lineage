da83c9bebc02e81047d22049f261379695f03a1b
yehonatan2020 <yehonatan@yandex.ru>
2024-02-02 10:15:37 +0000

    Add our own fonts

--- config/common_full.mk
+++ config/common_full.mk
@@ -18,7 +18,12 @@ PRODUCT_PACKAGES += \
     zstd
 
 # Fonts
+LOCAL_PATH := vendor/lineage/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
 PRODUCT_PACKAGES += \
-    fonts_customization.xml \
     FontLatoOverlay \
     FontRubikOverlay
