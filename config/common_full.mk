0f3416ac8d534de60239b2b5a175fb9e935fbb9c
yehonatan2020 <yehonatan@yandex.ru>
2024-06-09 00:30:04 +0100

    fix compile

--- config/common_full.mk
+++ config/common_full.mk
@@ -18,11 +18,10 @@ PRODUCT_PACKAGES += \
     zstd
 
 # Fonts
-LOCAL_PATH := vendor/lineage/fonts
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontLatoOverlay \
+    FontRubikOverlay
 
 PRODUCT_PACKAGES += \
     FontLatoOverlay \
