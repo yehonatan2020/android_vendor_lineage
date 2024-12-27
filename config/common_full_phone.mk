46d683d7f24a818948ea5ce4ffb1aafb3534fc56
yehonatan2020 <yehonatan@yandex.ru>
2024-12-27 12:50:17 +0000

    common_full_phone: Correct point-to destinations

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -11,10 +11,10 @@ $(call inherit-product, vendor/lineage/config/telephony.mk)
 WITH_GMS ?= true
 ifeq ($(WITH_GMS),true)
 ifeq ($(TARGET_USES_MINI_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_mini.mk)
+$(call inherit-product, vendor/lineage/config/gms_mini.mk)
 else ifeq ($(TARGET_USES_PICO_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_pico.mk)
+$(call inherit-product, vendor/lineage/config/gms_pico.mk)
 else
-$(call inherit-product, vendor/gms/gms_full.mk)
+$(call inherit-product, vendor/lineage/config/gms_full.mk)
 endif
 endif
